175c563b261c765032b4e48ab75ab3fca1d3eb32
kmrshubham123 <kmrshubham123@gmail.com>
2021-05-26 17:02:25 +0530

    [Kumar shubham]Added : calculating Part time empwage

--- EmpWageComputation/EmpWageComputation/EmployeeWage.cs
+++ EmpWageComputation/EmpWageComputation/EmployeeWage.cs
@@ -7,18 +7,23 @@ namespace EmpWageComputation
     public class EmployeeWage
     {
         //constant
-        const int IS_FULL_TIME = 1;
+        const int IS_FULL_TIME = 2;
         const int EMP_RATE_PER_HOURS = 20;
+        const int IS_PART_TIME = 1;
         //variable
         int empHrs = 0;
         int empWage = 0;
-        public void EmpWage()
+        public void TotalEmpWage()
         {
             Random random = new Random();
             int empCheck= random.Next(0, 2);
 
-            if (empCheck == IS_FULL_TIME)
+            if (empCheck == IS_PART_TIME)
             
+            {
+                empHrs = 4;
+            }
+            else if (empCheck == IS_FULL_TIME)
             {
                 empHrs = 8;
             }
