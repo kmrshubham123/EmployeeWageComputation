7cd9431b6b7de99f127cede54b214d5beebaae9f
kmrshubham123 <kmrshubham123@gmail.com>
2021-05-26 18:32:17 +0530

    [Kumar Shubham]Added: calculating wages till No. of working Days or total working Hrs per month

--- EmpWageComputation/EmpWageComputation/EmployeeWage.cs
+++ EmpWageComputation/EmpWageComputation/EmployeeWage.cs
@@ -10,8 +10,8 @@ namespace EmpWageComputation
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOURS = 20;
         public const int IS_PART_TIME = 1;
-        public const int NUM_OF_WORKING_DAYS = 2;
-        public const int MAX_HRS_IN_MONTH = 10;
+        public const int NUM_OF_WORKING_DAYS = 20;
+        public const int MAX_HRS_IN_MONTH = 100;
         //variable
         int empHrs = 0;
         int empWage = 0;
