b6b1d6216509224a7d3da620eff8c3cf6c98133f
kmrshubham123 <kmrshubham123@gmail.com>
2021-05-26 16:48:46 +0530

    [Kumar Shubham]Add : calculating Employee Wage

--- EmpWageComputation/EmpWageComputation/EmployeeWage.cs
+++ EmpWageComputation/EmpWageComputation/EmployeeWage.cs
@@ -6,16 +6,29 @@ namespace EmpWageComputation
 {
     public class EmployeeWage
     {
-        const int IS_PRESENT = 1;
-        public void Attendance()
+        //constant
+        const int IS_FULL_TIME = 1;
+        const int EMP_RATE_PER_HOURS = 20;
+        //variable
+        int empHrs = 0;
+        int empWage = 0;
+        public void EmpWage()
         {
             Random random = new Random();
             int empCheck= random.Next(0, 2);
 
-            if (empCheck == IS_PRESENT)
-                Console.WriteLine("Employee is Present");
+            if (empCheck == IS_FULL_TIME)
+            
+            {
+                empHrs = 8;
+            }
             else
-                Console.WriteLine("Employee is Absent");
+            {
+                empHrs = 0;
+            }
+
+            empWage = empHrs * EMP_RATE_PER_HOURS;
+            Console.WriteLine("Emp Wage :-" +empWage);
         }
 
     }
