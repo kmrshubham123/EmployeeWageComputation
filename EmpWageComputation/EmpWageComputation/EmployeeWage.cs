2498eac15634975f08186968094eca9105b0da98
kmrshubham123 <kmrshubham123@gmail.com>
2021-05-26 17:35:07 +0530

    [Kumar Shubham]Added: calculating wages for 20 day in a month

--- EmpWageComputation/EmpWageComputation/EmployeeWage.cs
+++ EmpWageComputation/EmpWageComputation/EmployeeWage.cs
@@ -7,34 +7,43 @@ namespace EmpWageComputation
     public class EmployeeWage
     {
         //constant
-        const int IS_FULL_TIME = 2;
-        const int EMP_RATE_PER_HOURS = 20;
-        const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOURS = 20;
+        public const int IS_PART_TIME = 1;
+        public const int NUM_OF_WORKING_DAYS = 2;
         //variable
         int empHrs = 0;
         int empWage = 0;
-        public void CaseEmpWage()
+        int totalEmpWage = 0;
+        
+       
+        public void MonthEmpWage()
         {
             Random random = new Random();
             int empCheck= random.Next(0, 2);
-            switch(empCheck)
-            {
-                case IS_PART_TIME:
+            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+
+              switch (empCheck)
+              {
+                  case IS_PART_TIME:
                     empHrs = 4;
                     break;
 
-                case IS_FULL_TIME:
+                  case IS_FULL_TIME:
                     empHrs = 8;
                     break;
 
-                default:
+                 default:
                     empHrs = 0;
                     break;
 
-            }
+              }
 
             empWage = empHrs * EMP_RATE_PER_HOURS;
+            totalEmpWage += empWage;
             Console.WriteLine("Emp Wage :-" +empWage);
+
+            Console.WriteLine("Total Emp Wage :-" + totalEmpWage);
         }
 
     }
