09c376f264c222ab3deb2e71290ce76ba84f823a
kmrshubham123 <kmrshubham123@gmail.com>
2021-06-07 16:56:25 +0530

    [Kumar Shubham]Added:Refactor to have a list of companies to manage Employee Wage

--- EmpWageComputation/EmpWageComputation/Program.cs
+++ EmpWageComputation/EmpWageComputation/Program.cs
@@ -9,8 +9,9 @@ namespace EmployeeWage
         {
             Console.WriteLine("Welcome to Employee Wage Computation");
             EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
-            empWageBuilder.addCompanyEmpWage("BigBazaar", 20, 10, 10);
-            empWageBuilder.addCompanyEmpWage("Trends", 20, 10, 10);
+            empWageBuilder.addCompanyEmpWage("BigBazaar", 20, 2, 10);
+            empWageBuilder.addCompanyEmpWage("Trends", 10, 4, 10);
+            empWageBuilder.addCompanyEmpWage("FaceBook", 20, 10, 10);
             empWageBuilder.computeEmpWage();
 
 
